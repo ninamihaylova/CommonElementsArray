bea36101c62612b7fb78572891684016e6ae2de3
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-25 16:44:43 +0300

    Initial commit

---
+++ CommonElementsArray/Program.cs
@@ -0,0 +1,26 @@
+﻿class CommonElementsArray
+{
+    static void Main()
+    {
+        
+        int[] numbers1 = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray();
+        int[] numbers2 = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int i = 0; i < numbers1.Length; i++)
+        {
+            int currentNumber=numbers1[i];
+            foreach(int number in numbers2){
+                if (currentNumber==number){
+                    Console.Write($"{number} ");
+                    break; 
+                } 
+            }
+        } 
+    }
+        }
